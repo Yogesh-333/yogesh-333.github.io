0d49ba41bd89227bf31fa3dc154744dc1c4c3846
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-11-06 21:14:53 -0500

    project detail function added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "205436773"
+    serviceWorkerVersion: "4032991440"
   }
 });
